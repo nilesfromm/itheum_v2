1e7204a8bac4549e55f374a5f4b3745826b6a378
Niles <nsfromm@gmail.com>
2022-01-10 17:31:23 -0500

    interactions

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import "./style.css";
 import { Itheum } from "./itheum";
 
 window.onload = () => {
-  new Itheum(document.getElementsByClassName("viewport")[0] as HTMLElement, 100, 100);
+  new Itheum(document.getElementsByClassName("viewport")[0] as HTMLElement, 1500, 2000);
 };
